416d4cd1b793a3b3ebfc69ba57ec5ad2f1831439
JoseG2003-g <jjoseggg2001@gmail.com>
2025-11-08 13:02:06 -0500

    Create Shopkeeper.py

---
+++ Shopkeeper.py
@@ -0,0 +1,50 @@
+
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # load API key 
+api_key = os.getenv("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")
+
+client = OpenAI(api_key= api_key)
+
+
+
+
+class MainCharacter:
+
+    def __init__(self, name):
+    
+        self.name = name
+        self.health = 100
+        self.money = 45
+        self.armor = 20
+
+class Enemy:
+
+    def __init__(self, name, health, armor):
+
+        self.name = name
+        self.health = health
+        self.armor = armor
+
+class Shopkeeper:
+
+    def __init__(self, name, personality):
+
+        self.name = name
+        self.personality = personality
+
+
+
+
+
+
+def main():
+
+    print("")
+
+Hero1 = MainCharacter("Player")
+
+Cyclops = Enemy ("Cyclops", 120, 10 )
+
